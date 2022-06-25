722aa2c4ccbf64349ac8a6bb1b330dfbbfdc849e
Hafizul <hafizulm.noor@gmail.com>
2022-06-25 12:21:16 +0800

    test 2

--- src/index.ts
+++ src/index.ts
@@ -1,33 +1,41 @@
 const fs = require("fs");
-
+// fs.promises
+// implement data typing
 let result = [];
+
+//Read file
 var data = fs.readFileSync("files/material.csv");
 
 //Split rows
-var data_array = data.toString().split("\r");
-var data_header = data_array[0].split(",");
+var data_array = data.toString().split("\n");
+//console.log(data_array);
 
+//Split header
+var data_header = data_array[0].split(",");
+//console.log(data_header);
+
+//Read each data.
 for (let i = 1; i < data_array.length - 1; i++) {
-  //console.log(i);
   let data_obj = {};
   let str_temp = data_array[i];
   let data_str = "";
-  console.log(str_temp);
+  //console.log(str_temp);
 
   let flag = 0;
+  //Insert insert each data in " "
   for (let ch of str_temp) {
     if (ch === '"' && flag === 0) {
       flag = 1;
     } else if (ch === '"' && flag == 1) flag = 0;
-    if (ch === ", " && flag === 0) ch = "|";
+    if (ch === "," && flag === 0) ch = "|";
     if (ch !== '"') data_str += ch;
   }
 
+  //Split data with others.
   let properties = data_str.split("|");
-  console.log(properties);
 
   for (let j in data_header) {
-    if (properties[j].includes(", ")) {
+    if (properties[j].includes(",")) {
       data_obj[data_header[j]] = properties[j]
         .split(",")
         .map((item) => item.trim());
@@ -35,7 +43,18 @@ for (let i = 1; i < data_array.length - 1; i++) {
   }
   result.push(data_obj);
 }
+//result.slice(2, 4);
+// filter result
+//var sortedRes = result.filter();
+console.log(result[0].Sample);
 
-let json = JSON.stringify(result);
-console.log(json);
-fs.writeFileSync("files/output.json", json);
+// const newRes = (...arrCsv) => {
+//   delete arrCsv["Sample"];
+// };
+
+// console.log(newRes(result));
+//req input from user
+
+//let json = JSON.stringify(result);
+
+//fs.writeFileSync("files/output.json", json);
